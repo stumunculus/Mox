9184c979bd3ce309f553ee1010997547c1279058
alxjrvs <alxjrvs@gmail.com>
2012-08-26 16:44:03 -0400

    Namespace'd out, dynamic instances.

--- lib/card.rb
+++ lib/card.rb
@@ -1,19 +1,50 @@
-class Card
-  attr_accessor :name, :mana_cost, :set, :text, :type
 
-  def initialize
-    @type = self.class
+module CreatureStuff
+ attr_accessor :power, :toughness
+
+  def pt
+    "#{self.power}/#{self.toughness}"
+  end
+end 
+
+module ArtifactStuff
+end
+
+module EnchantmentStuff
+end
+
+module LandStuff
+end 
+
+module PlaneswalkerStuff
+  attr_accessor :loyalty
+end
+
+
+class Card
+  attr_accessor :supertype, :name, :mana_cost, :set, :text, :type, :subtype, :rarity, :collectors_no, :flavor, :artist
+  def initialize args 
+    args.each do |k,v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+    @type = self.class  
   end
 
-  def cmc(cost)
+  def cmc
     cmc = 0
-    cost.split("").each do |t|
+    self.mana_cost.split("").each do |t|
       cmc += t.to_i
       cmc += 1 if t =~ /[WUBRG]/
     end
     cmc
   end
 
+  def color_id
+    color_id = []
+    self.mana_cost.split("").each do |t| 
+      
+    end
+  end
 end
 
 class Permanent < Card
@@ -24,13 +55,28 @@ end
 
 
 class Creature < Permanent
-  attr_accessor :power, :toughness, :creature_type
-
-  def pt
-    "#{self.power}/#{self.toughness}"
-  end
+ include CreatureStuff
 end
 
 class Instant < NonPermanent
 end
 
+class Artifact < Permanent
+  include ArtifactStuff
+end
+
+class Planeswalker < Permanent 
+  include PlaneswalkerStuff
+end
+
+class Land < Permanent 
+  
+end
+ 
+class Enchantment < Permanent
+end
+
+class ArtifactCreature < Permanent 
+  include CreatureStuff
+  include ArtifactStuff 
+end
